ba7e504f96c9498e6fea55316462c92b6fdaf066
Asutosh Dalei <asutoshdalei@gmail.com>
2020-12-12 02:10:46 +0530

    Major changes performed

--- button_control.ino
+++ button_control.ino
@@ -13,6 +13,7 @@ int LRmotorpin1 = 4;
 int LRmotorpin2 = 5;
 int LRen = 11;
 const int t = 200;
+int curr_flr = 0;
 void setup()
 {
   servo_test.attach(9);
@@ -51,7 +52,7 @@ void forward()
 {
   digitalWrite(UDen, LOW);
   digitalWrite(LRen, HIGH);
-  Serial.println("going fwd");
+  Serial.println("going right");
   digitalWrite(UDmotorpin1, LOW);
   digitalWrite(UDmotorpin2, LOW);
   delay(t);
@@ -63,7 +64,7 @@ void back()
 {
   //digitalWrite(UDen, LOW);
   //digitalWrite(LRen, HIGH);
-  Serial.println("going back");
+  Serial.println("going left");
   digitalWrite(UDmotorpin1, LOW);
   digitalWrite(UDmotorpin2, LOW);
   delay(t);
@@ -99,70 +100,75 @@ void clicks()
 }
 
 void loop() {
-  // while (Serial.available()==0){}             // wait for user input
-  //flr_num = Serial.parseInt();
-  int  flr_num = 4;
-  if (flr_num < 7 && flr_num > 0)
+  Serial.println("Enter floor");
+  while (Serial.available()==0){}             // wait for user input
+  int flr_num = Serial.parseInt();
+
+  if (flr_num <= 7 && flr_num >= 0)
   {
-    if (flr_num % 2 == 0)
+    //x-axis movement
+    if (curr_flr%2 == 0)
     {
-      for (int i = 0; i < flr_num / 2; i++)
+      if (flr_num % 2 != 0)
       {
-        Serial.println("y axis - UP");
-        up();
-        delay(2000);
-        stops();
-        delay(2000);
-      }
-      Serial.println("click");
-      clicks();
-      delay(2000);
-      for (int i = 0; i < flr_num / 2; i++)
-      {
-        Serial.println("y axis - DOWN");
-        down();
-        delay(2000);
-        stops();
+        forward();
         delay(2000);
       }
     }
     else
     {
-      Serial.println("x axis - FWD");
-      forward();
-      delay(2000);
-      stops();
-      delay(2000);
-      for (int i = 0; i < flr_num / 2; i++)
+      if (flr_num % 2 == 0)
       {
-        Serial.println("y axis - UP");
+        back();
+        delay(2000);
+      }
+    }
+    //y-axis movement
+    int diff = flr_num - curr_flr;
+    if  (curr_flr == 0 || flr_num == 0)
+    {
+      if(diff > 0){
+      for(int i =0; i < abs(diff)/2; i++)
+      {
         up();
         delay(2000);
-        stops();
-        delay(2000);
       }
-      Serial.println("click");
       clicks();
-      delay(2000);
-      for (int i = 0; i < flr_num / 2; i++)
+    }
+    else
+    {
+      for(int i =0; i < abs(diff)/2; i++)
       {
-        Serial.println("y axis - DOWN");
         down();
         delay(2000);
-        stops();
-        delay(2000);
       }
-      Serial.println("x axis - BACK");
-      back();
-      delay(2000);
-      stops();
-      delay(2000);
-
+      clicks();
+    }
+    } 
+    else
+    {
+      if(diff > 0){
+        for(int i =0; i< (abs(diff)+1)/2; i++)
+        {
+          up();
+          delay(2000);
+        }
+        clicks();
+      }
+      else
+      {
+        for(int i =0; i< (abs(diff)+1)/2; i++)
+        {
+          down();
+          delay(2000);
+        }
+        clicks();
+      }
     }
   }
   else
   {
     Serial.println("INVALID FLOOR");
   }
-  exit(0);
+  curr_flr = flr_num;
 }
