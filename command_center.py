7216aa78f62c8bddd7e942e7f258530568866966
Asutosh Dalei <asutoshdalei@gmail.com>
2020-12-12 00:42:00 +0530

    Addition of packages and TTS module

--- command_center.py
+++ command_center.py
@@ -1,2 +1,17 @@
 #This piece of code aims to initiate the lift operation
+
+#Importing all necessary packages : V1
 import speech_recognition as sr
+r = sr.Recognizer()
+from time import sleep
+import pyttsx3
+engine = pyttsx3.init()
+
+def say(r):      #This function based a TTS engine to convert a given text to speech  
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', 125)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.say(r)
+    engine.runAndWait()
+
