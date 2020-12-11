ea2bc5f682d6e493a6b81e37d2fb0b7864a5b3ef
Asutosh Dalei <asutoshdalei@gmail.com>
2020-12-12 00:58:09 +0530

    Addition of speech recognition module

--- command_center.py
+++ command_center.py
@@ -15,3 +15,19 @@ def say(r):      #This function based a TTS engine to convert a given text to sp
     engine.say(r)
     engine.runAndWait()
 
+def listen():    #This function is to listen to a person talking. This function recognises the word or sentence told by the user.
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Kindly speak now:")
+        say("Kindly speak now")
+        audio_text = r.listen(source)
+        sleep(3)
+        print("Time over, thanks")
+        try:
+            say("Did you say: "+r.recognize_google(audio_text))
+            print(r.recognize_google(audio_text))
+            return(r.recognize_google(audio_text))
+        except:
+            say("Sorry, I did not get that")
+            return("sorry")
+
