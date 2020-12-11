a4d4dea9640e0702a60711ff4b356c7f2e4f0847
Asutosh Dalei <asutoshdalei@gmail.com>
2020-12-12 03:29:10 +0530

    Version 1 ready

--- button_control_v1.ino
+++ button_control_v1.ino
@@ -3,8 +3,7 @@
 Servo servo_test;
 
 int angle = 0;
-
-//int flr_num;
+//----------------------------------------INITIALISATION-----------------------------------------------
 int UDmotorpin1 = 2;
 int UDmotorpin2 = 3;
 int UDen = 10;
@@ -14,6 +13,9 @@ int LRmotorpin2 = 5;
 int LRen = 11;
 const int t = 200;
 int curr_flr = 0;
+
+//-------------------------------------------SETUP------------------------------------------------------
+
 void setup()
 {
   servo_test.attach(9);
@@ -23,6 +25,8 @@ void setup()
   pinMode(LRmotorpin2, OUTPUT);
   Serial.begin(9600);
 }
+
+// MOTOR CONTROL
 void up()
 {
   Serial.println("moving up");
@@ -33,6 +37,8 @@ void up()
   delay(t);
   digitalWrite(LRmotorpin1, LOW);
   digitalWrite(LRmotorpin2, LOW);
+  curr_flr += 2;
+  Serial.println(curr_flr);
 
 }
 void down()
@@ -45,7 +51,8 @@ void down()
   delay(t);
   digitalWrite(LRmotorpin1, LOW);
   digitalWrite(LRmotorpin2, LOW);
-
+  curr_flr -= 2;
+  Serial.println(curr_flr);
 }
 
 void forward()
@@ -58,19 +65,20 @@ void forward()
   delay(t);
   digitalWrite(LRmotorpin1, HIGH);
   digitalWrite(LRmotorpin2, LOW);
+  curr_flr += 1;
+  Serial.println(curr_flr);
 
 }
 void back()
 {
-  //digitalWrite(UDen, LOW);
-  //digitalWrite(LRen, HIGH);
   Serial.println("going left");
   digitalWrite(UDmotorpin1, LOW);
   digitalWrite(UDmotorpin2, LOW);
   delay(t);
   digitalWrite(LRmotorpin1, LOW);
   digitalWrite(LRmotorpin2, HIGH);
-
+  curr_flr -= 1;
+  Serial.println(curr_flr);
 }
 void stops()
 {
@@ -80,7 +88,7 @@ void stops()
   digitalWrite(LRmotorpin2, LOW);
 }
 
-void clicks()
+void clicks() //Press Button
 {
   for (angle = 0; angle < 180; angle += 1)
   {
@@ -95,16 +103,18 @@ void clicks()
     servo_test.write(angle);
     delay(5);
   }
-
   Serial.println("click button");
 }
+
+//----------------------------------------LOOP---------------------------------------
 
 void loop() {
   Serial.println("Enter floor");
   while (Serial.available()==0){}             // wait for user input
   int flr_num = Serial.parseInt();
+  Serial.setTimeout(43200000);    //if no button is pressed for 12 hrs, lift goes to zero
 
-  if (flr_num <= 7 && flr_num >= 0)
+  if (flr_num <= 7 && flr_num >= 0) //check for valid floor input
   {
     //x-axis movement
     if (curr_flr%2 == 0)
@@ -123,6 +133,9 @@ void loop() {
         delay(2000);
       }
     }
+    if(curr_flr == flr_num){
+        clicks();
+      }
     //y-axis movement
     int diff = flr_num - curr_flr;
     if  (curr_flr == 0 || flr_num == 0)
@@ -133,7 +146,9 @@ void loop() {
         up();
         delay(2000);
       }
-      clicks();
+      if(curr_flr == flr_num){
+        clicks();
+      }
     }
     else
     {
@@ -142,7 +157,9 @@ void loop() {
         down();
         delay(2000);
       }
-      clicks();
+      if(curr_flr == flr_num){
+        clicks();
+      }
     }
     } 
     else
@@ -153,7 +170,9 @@ void loop() {
           up();
           delay(2000);
         }
+        if(curr_flr == flr_num){
         clicks();
+      }
       }
       else
       {
@@ -162,13 +181,14 @@ void loop() {
           down();
           delay(2000);
         }
+        if(curr_flr == flr_num){
         clicks();
+      }
       }
     }
   }
   else
   {
-    Serial.println("INVALID FLOOR");
+    Serial.println("INVALID FLOOR, Try Again"); //print in OLED
   }
-  curr_flr = flr_num;
 }
